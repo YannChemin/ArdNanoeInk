e31278c737e656e775a4e62855bc44446188555c
Yann Chemin <ychemin@gmail.com>
2022-09-24 10:48:40 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -11,20 +11,21 @@ cp $inputxbm pnj.xbm
 val=$(echo $inputxbm | sed 's/.xbm//')
 sed -i "s/"$val"/pnj/g" pnj.xbm
 
-# Start by ensuring ttyUSB0 on MacBook Pro
 rootdir=$PWD
-#Enable ttyUSB0
-cd ~/dev/CH341SER
-bash compile.sh
-#come back
-cd $rootdir
+# THIS IS OPTIONAL: CH340 USB driver issue
+## Start by ensuring ttyUSB0 on MacBook Pro
+##Enable ttyUSB0
+#cd ~/dev/CH341SER
+#bash compile.sh
+##come back
+#cd $rootdir
 
 #Create/Clean tmpdir
 tmpdir=/tmp/arduino_build_yann
 mkdir -p $tmpdir
 cd $tmpdir
 rm -Rf *
-cd ..
+cd $rootdir
 
 #Build
 arduino-builder -dump-prefs -logger=machine -hardware /usr/share/arduino/hardware -hardware /home/yann/.arduino15/packages -tools /usr/share/arduino/hardware/tools/avr -tools /home/yann/.arduino15/packages -libraries /home/yann/Arduino/libraries -fqbn=arduino:avr:nano:cpu=atmega328old -build-path $tmpdir -warnings=none -prefs=build.warn_data_percentage=75 -verbose $rootdir/dnd.ino
